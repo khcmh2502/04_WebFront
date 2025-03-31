345ef73d74c0a2b20dd58bf007506acf3255d464
khcmh2502 <khcmh2502@gmail.com>
2025-03-31 17:29:23 +0900

    이벤트 예제 답안

--- 3_Javascript/js/08_이벤트.js
+++ 3_Javascript/js/08_이벤트.js
@@ -104,3 +104,21 @@ test2.addEventListener("click", function() {
   // == 이벤트가 발생한 요소
   this.innerText = count;
 });
+
+
+const input3 = document.querySelector("#input3");
+const box3 = document.querySelector("#box3");
+
+input3.addEventListener("keyup", function(e) {
+
+  if(e.key == "Enter") {
+    box3.style.backgroundColor = input3.value;
+  }
+});
+
+box3.addEventListener("click", function(e) {
+
+  // e.target : 이벤트가 발생한 대상 요소 == this
+
+  alert(`배경색 : ${e.target.style.backgroundColor}`);
+});
